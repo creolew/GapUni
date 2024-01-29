842f527807f675cb67f73f157fdddc8029c718a5
tottran <tottianddona@gmail.com>
2024-01-29 23:31:28 +0700

    add mysql code

---
+++ src/databases/mysql.sql
@@ -0,0 +1,11 @@
+/*
+Enter your query here.
+*/
+select 
+    case when g.grade >= 8 then s.name else NULL end as names
+    ,g.grade
+    ,s.marks
+from students s
+join grades g
+    on s.marks between g.min_mark and g.max_mark
+order by g.grade desc, names, s.marks
